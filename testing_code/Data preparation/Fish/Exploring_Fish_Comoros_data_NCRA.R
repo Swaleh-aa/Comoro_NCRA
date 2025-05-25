4b083a4eb59be7083255b0c22f2fb2583265d3ed
Swaleh_aa <51902757+Swaleh-aa@users.noreply.github.com>
2025-05-25 20:08:21 +0300

    Exploring criteria D results

--- testing_code/Data preparation/Fish/Exploring_Fish_Comoros_data_NCRA.R
+++ testing_code/Data preparation/Fish/Exploring_Fish_Comoros_data_NCRA.R
@@ -296,7 +296,42 @@ ggplot(Fish_NCRA_complete, aes(x = Management_level, y = biomass.kg.ha.)) +
 ggsave("../figures/Fish/Explore_Regional_Management_Biomass_Distribution_Families.png", width = 10, height = 6, dpi = 300)
 
 
+# Criteria D
 
+load("data_intermediate/create_criterion_d_iterations_fish.rda")
+
+create_criterion_iteration_summary_fish$status <- factor(
+  create_criterion_iteration_summary_fish$status,
+  levels = c("NT","LC","VU","EN","CR")
+)
+
+# Create the plot
+ggplot(create_criterion_iteration_summary_fish, 
+       aes(x = fish, y = percent, fill = status)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.7) +
+  scale_fill_manual(values = c(
+    "CR" = "red",
+    "EN" = "orange",
+    "VU" = "yellow",
+    "LC" = "#1a9641",
+    "NT" = "green"
+  )) +
+  facet_grid(Method ~ Ecoregion) +
+  labs(y = "Percent") +
+  theme_bw() +
+  theme(axis.title = element_blank())
+
+
+ggsave("figures/Fish/Explore_iterations_subnational_Families.png", width = 10, height = 6, dpi = 300)
+
+
+
+
+
+
+
+
+ 
 
 
 
